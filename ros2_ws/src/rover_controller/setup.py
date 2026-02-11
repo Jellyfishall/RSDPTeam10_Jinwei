d0c3de9614f7b72f3a7adab960e0338f3d399a11
Alex Inch <alexinch96@yahoo.com>
2026-02-11 16:34:24 +0000

    Add numpy as a dependency

--- ros2_ws/src/rover_controller/setup.py
+++ ros2_ws/src/rover_controller/setup.py
@@ -1,30 +1,29 @@
 from setuptools import find_packages, setup
 
-package_name = 'rover_controller'
+package_name = "rover_controller"
 
 setup(
     name=package_name,
-    version='0.0.0',
-    packages=find_packages(exclude=['test']),
+    version="0.0.0",
+    packages=find_packages(exclude=["test"]),
     data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
-        ('share/' + package_name, ['package.xml']),
+        ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
+        ("share/" + package_name, ["package.xml"]),
     ],
-    install_requires=['setuptools'],
+    install_requires=["setuptools", "numpy"],
     zip_safe=True,
-    maintainer='laptop20',
-    maintainer_email='alexinch96@yahoo.com',
-    description='TODO: Package description',
-    license='TODO: License declaration',
+    maintainer="laptop20",
+    maintainer_email="alexinch96@yahoo.com",
+    description="TODO: Package description",
+    license="TODO: License declaration",
     extras_require={
-        'test': [
-            'pytest',
+        "test": [
+            "pytest",
         ],
     },
     entry_points={
-        'console_scripts': [
-            'rover_controller = rover_controller.rover_controller:main'
+        "console_scripts": [
+            "rover_controller = rover_controller.rover_controller:main"
         ],
     },
 )
